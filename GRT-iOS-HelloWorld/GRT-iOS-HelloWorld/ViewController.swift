0ece4a2d27b876778f87a7dcab4f9064f6d73f35
Nicholas Arner <nicholasarner@gmail.com>
2017-08-17 10:07:13 -0400

    Creating HelloWorld app, and a workspace so that I can edit the framework + the app in the same project.

---
+++ GRT-iOS-HelloWorld/GRT-iOS-HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GRT-iOS-HelloWorld
+//
+//  Created by Nicholas Arner on 8/17/17.
+//  Copyright © 2017 Nicholas Arner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GRT-iOS-HelloWorld/GRT-iOS-HelloWorldTests/GRT_iOS_HelloWorldTests.swift
@@ -0,0 +1,36 @@
+//
+//  GRT_iOS_HelloWorldTests.swift
+//  GRT-iOS-HelloWorldTests
+//
+//  Created by Nicholas Arner on 8/17/17.
+//  Copyright © 2017 Nicholas Arner. All rights reserved.
+//
+
+import XCTest
+@testable import GRT_iOS_HelloWorld
+
+class GRT_iOS_HelloWorldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
